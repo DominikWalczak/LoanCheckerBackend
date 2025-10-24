ce9d5fad76ace594ec2bfd4e2eefd4cdcf67e20e
Dominik Walczak <dominik.walczak2@gmail.com>
2025-10-24 16:12:57 +0200

    łączę backend z bazą danych oraz frontendem

---
+++ Desktop/MobileApp/praktyki2_back/my-loan-checker-backend-app/backend/app.js
@@ -0,0 +1,27 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import db from "./db/db.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// app.get("/", (req, res) => {
+//   res.send("Serwer Express działa i jest połączony z MySQL!");
+// });
+
+app.get("/", (req, res) => {
+  db.query("SELECT * FROM users", (err, results) => {
+    if (err) {
+      console.error("Błąd zapytania:", err);
+      return res.status(500).json({ error: "Błąd bazy danych" });
+    }
+    res.json(results);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Serwer działa na porcie ${PORT}`));
